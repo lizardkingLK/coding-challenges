cc5a049e70d79c9494fff3c08bd3ec2734b7805a
lizardkingLK <chansanfdo@gmail.com>
2025-05-10 17:25:28 +0530

    Create your own json parser
    - Step 0-1 complete
    
    - Empty json and curly braces are validated
    
    - Refactored code structure

--- jpTool/src/jpTool.Core/Validator.cs
+++ jpTool/src/jpTool.Core/Validator.cs
@@ -4,7 +4,7 @@ namespace jpTool.Core
 
     public static class Validator
     {
-        public static Result<bool> ValidateJson(string filePath)
+        public static Result<bool> ValidateJsonFile(string filePath)
         {
             string jsonString = File.ReadAllText(filePath).Trim();
             if (string.IsNullOrEmpty(jsonString))
@@ -12,6 +12,11 @@ namespace jpTool.Core
                 return new Result<bool>(false, ERROR_FILE_IS_EMPTY);
             }
 
+            return ValidateJsonString(jsonString);
+        }
+
+        public static Result<bool> ValidateJsonString(string jsonString)
+        {
             if (!jsonString.StartsWith('{') || !jsonString.EndsWith('}'))
             {
                 return new Result<bool>(false, ERROR_INVALID_JSON_FOUND);
--- jpTool/tests/jpTool.Core.Tests/Tests.Step1.cs
+++ jpTool/tests/jpTool.Core.Tests/Tests.Step1.cs
@@ -5,13 +5,15 @@ public class TestsStep1
     [Theory]
     [InlineData(@"data\step1\invalid.json", false)]
     [InlineData(@"data\step1\valid.json", true)]
+    [InlineData(@"data\step2\invalid.json", false)]
+    [InlineData(@"data\step2\invalid2.json", false)]
     public void Should_Test_For_Valid_Json_In_Step_1(string filePath, bool expectedResult)
     {
         // Arrange
         string absoluteFilePath = GetAbsoluteFilePath(filePath);
 
         // Act
-        var actualResult = Validator.ValidateJson(absoluteFilePath);
+        var actualResult = Validator.ValidateJsonFile(absoluteFilePath);
 
         // Assert
         Assert.Equal(expectedResult, actualResult.Data);
