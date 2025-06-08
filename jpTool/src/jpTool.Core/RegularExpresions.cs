44ba32d14495fe3a08dbf4e9c4e1049028553563
lizardkingLK <chansanfdo@gmail.com>
2025-06-08 16:53:09 +0530

    Added regular expressions
    - Regular expressions are used to match numerical and string literal with quotes as the start of the json
    
    - This is as per new json standard
    
    - New json standard allows non-object and non-array literals that aren't strings without literals

---
+++ jpTool/src/jpTool.Core/RegularExpresions.cs
@@ -0,0 +1,13 @@
+namespace jpTool.Core;
+
+internal static partial class RegularExpressions
+{
+    [GeneratedRegex(@"(([\n\r\t\s\b\f])|(\\u+[0-9aA-fF]{4}))")]
+    internal static partial Regex RegexForSpecialAndWhiteSpace();
+
+    [GeneratedRegex(@"^[-]{0,1}(0|([1-9]{1})([0-9]{0,}))(\.[0-9]{1,}){0,1}([eE][-]{0,1}[0-9]{1,}){0,1}$")]
+    internal static partial Regex RegexForMatchScientificNumber();
+
+    [GeneratedRegex(@"^(\"")(.){1,}(\1)$")]
+    internal static partial Regex RegexForMatchQuotedLineOfText();
+}
