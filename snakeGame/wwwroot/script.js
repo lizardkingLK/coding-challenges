d4ddd99cd793cd415195c185963dc727114460ff
lizardkingLK <chansanfdo@gmail.com>
2025-07-27 23:35:24 +0530

    Updated snake game
    - Removed wwwroot

--- snakeGame/wwwroot/script.js
+++
@@ -1,7 +0,0 @@
-const apiUrl = "http://localhost:5000/events";
-const eventSource = new EventSource(apiUrl);
-const messages = document.querySelector("#messages");
-
-eventSource.onmessage = ({ data }) => {
-    messages.innerHTML += `<br/> ${data}`;
-}
