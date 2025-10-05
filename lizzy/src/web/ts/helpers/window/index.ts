98d521b7184e705c7c93841c1f8e5349e1adf774
lizardkinglk <chansanfdo@gmail.com>
2025-10-05 11:35:27 +0530

    Updated lizzy app
    - Removed client app for web
    
    - This app will be developed as a cli utility

--- lizzy/src/web/ts/helpers/window/index.ts
+++
@@ -1,8 +0,0 @@
-export class WindowHelper {
-  getSearchParams() {
-    const searchString = window.location.search;
-    const urlSearchParams = new URLSearchParams(searchString);
-
-    return Object.fromEntries(urlSearchParams);
-  }
-}
