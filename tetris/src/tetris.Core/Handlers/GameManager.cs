0789a43cf4acc7a7b03008e1fbeb7c2faa9812e3
lizardkinglk <chansanfdo@gmail.com>
2025-11-09 23:24:23 +0530

    Updated tetris
    - Added game managers skeletons
    
    - Initialized base level game managements

--- tetris/src/tetris.Core/Handlers/GameManager.cs
+++ tetris/src/tetris.Core/Handlers/GameManager.cs
@@ -11,7 +11,11 @@ public class GameManager(Arguments arguments) : IManager
     private readonly HashMap<GameModeEnum, IManager> _game = new(
         (GameModeEnum.Classic, new ClassicGameManager(arguments)));
 
-    // TODO: initialize output that handles game outputs
+    // TODO: initialize output that handles game outputs. output means the display for game
+    //
+    // TODO: timed game mode means countdown of a 10 minutes and should score high scores
+    //
+    // TODO: scored game mode means the time it takes to beat 100 at score. and less time means high multiplier to score.
 
     public void New()
     {
