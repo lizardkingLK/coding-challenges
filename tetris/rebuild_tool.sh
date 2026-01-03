4a730c35aea388b323188620ffb732a435282b17
lizardkinglk <chansanfdo@gmail.com>
2026-01-03 19:54:46 +0530

    Updated tetris
    - Updated rebuild_tool.sh

--- tetris/rebuild_tool.sh
+++ tetris/rebuild_tool.sh
@@ -10,18 +10,26 @@ currentDirectory=$(pwd)
 toolName='tetris.Program'
 
 # uninstall if tool contains in the system
-if dotnet tool list --global $toolName | grep -q "$toolName"; then
-  installPlease=$(dotnet tool uninstall --global $toolName)
+containsTool=false
+if dotnet tool list --global | grep -q ${toolName,,}; then
+  containsTool=true
+fi
+
+# if exists uninstall
+if [ "$containsTool" = true ]; then
+  echo "Uninstalling existing global tool: $toolName"
+  dotnet tool uninstall --global "$toolName"
 fi
 
 # go to cli root
-cd ./src/$toolName
+cd "./src/$toolName" || { echo "error. invalid directory ./src/$toolName"; exit 1; }
 
 # package the solution
-packaged=$(dotnet pack)
+dotnet pack || { echo "error. dotnet pack failed"; exit 1; }
 
 # install the tool
-installed=$(dotnet tool install --global --add-source ./nupkg $toolName)
+echo "info. installing the tool globally from local package"
+dotnet tool install --global --add-source ./nupkg "$toolName" || { echo "error. install failed."; exit 1; }
 
 # set current directory as location
-cd $currentDirectory
+cd $currentDirectory || { echo "error. cannot return to original directory"; }
