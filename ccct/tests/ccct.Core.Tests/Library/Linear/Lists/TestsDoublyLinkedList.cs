0adcbef6271ee1b7f6b314d0bb7a69af21e801ab
lizardkinglk <chansanfdo@gmail.com>
2026-03-30 17:01:21 +0530

    Updated compression tool
    - Updated data structure tests

--- ccct/tests/ccct.Core.Tests/Library/Linear/Lists/TestsDoublyLinkedList.cs
+++ ccct/tests/ccct.Core.Tests/Library/Linear/Lists/TestsDoublyLinkedList.cs
@@ -1,5 +1,6 @@
 using ccct.Core.Library.Linear.Arrays;
 using ccct.Core.Library.Linear.Lists;
+using static ccct.Core.Shared.Errors;
 
 namespace ccct.Core.Tests.Library.Linear.Lists;
 
@@ -52,5 +53,106 @@ public class TestsDoublyLinkedList
         {
             Assert.Equal(values[i], val[i]);
         }
+    }
+
+    [Theory]
+    [InlineData(1, 2, 3)]
+    [InlineData(true, false, true)]
+    [InlineData("Hello", " ", "World", "!")]
+    [InlineData(1f, 2f, 3f, 1.2f, 2.3f, 3.4f)]
+    public void Should_RemoveFromFront_DoublyLinkedList(params object[] values)
+    {
+        // Arrange
+        DoublyLinkedList<object> list = new();
+        foreach (object value in values)
+        {
+            list.AddToFront(value);
+        }
+
+        // Act
+        object removedValue = list.RemoveFromFront().Value;
+
+        // Assert
+        Assert.Equal(values[^1], removedValue);
+    }
+
+    [Theory]
+    [InlineData(1)]
+    [InlineData(true)]
+    [InlineData("Hello")]
+    [InlineData(1f)]
+    public void Should_RemoveFromFront_DoublyLinkedList_Exception(params object[] values)
+    {
+        // Arrange
+        DoublyLinkedList<object> list = new();
+        foreach (object value in values)
+        {
+            list.AddToFront(value);
+        }
+
+        object removedValue = list.RemoveFromFront().Value;
+
+        // Act
+        void act()
+        {
+            removedValue = list.RemoveFromFront().Value;
+        }
+
+        ApplicationException exception = Assert.Throws<ApplicationException>(act);
+
+        // Assert
+        Assert.Equal(ErrorDoublyLinkedListCannotRemoveFromFront, exception.Message);
+    }
+
+
+    ///////////////////////////////////////////
+    /// 
+    [Theory]
+    [InlineData(1, 2, 3)]
+    [InlineData(true, false, true)]
+    [InlineData("Hello", " ", "World", "!")]
+    [InlineData(1f, 2f, 3f, 1.2f, 2.3f, 3.4f)]
+    public void Should_RemoveFromRear_DoublyLinkedList(params object[] values)
+    {
+        // Arrange
+        DoublyLinkedList<object> list = new();
+        foreach (object value in values)
+        {
+            list.AddToRear(value);
+        }
+
+        // Act
+        object removedValue = list.RemoveFromRear().Value;
+
+        // Assert
+        Assert.Equal(values[^1], removedValue);
+    }
+
+    [Theory]
+    [InlineData(1)]
+    [InlineData(true)]
+    [InlineData("Hello")]
+    [InlineData(1f)]
+    public void Should_RemoveFromRear_DoublyLinkedList_Exception(params object[] values)
+    {
+        // Arrange
+        DoublyLinkedList<object> list = new();
+        foreach (object value in values)
+        {
+            list.AddToRear(value);
+        }
+
+        object removedValue = list.RemoveFromRear().Value;
+
+        // Act
+        void act()
+        {
+            removedValue = list.RemoveFromRear().Value;
+        }
+
+        ApplicationException exception = Assert.Throws<ApplicationException>(act);
+
+        // Assert
+        Assert.Equal(ErrorDoublyLinkedListCannotRemoveFromRear, exception.Message);
     }
 }
